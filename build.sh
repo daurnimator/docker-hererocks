955c3e36190ecfb33ae7200a8fc48929f2cab4be
daurnimator <quae@daurnimator.com>
2018-07-28 15:42:35 +1000

    Make registry prefix configurable

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-docker build --target hererocks -t daurnimator/hererocks .
-docker build --target lua5.1 -t daurnimator/lua5.1 .
-docker build --target lua5.2 -t daurnimator/lua5.2 .
-docker build --target lua5.3 -t daurnimator/lua5.3 .
-docker build --target luajit2.0 -t daurnimator/luajit2.0 .
-docker build --target luajit2.1 -t daurnimator/luajit2.1 .
-docker build --target luacheck -t daurnimator/luacheck .
-docker push daurnimator/hererocks
-docker push daurnimator/lua5.1
-docker push daurnimator/lua5.2
-docker push daurnimator/lua5.3
-docker push daurnimator/luajit2.1
-docker push daurnimator/luacheck
+
+set -e
+
+: ${REGISTRY_PREFIX:=daurnimator/}
+
+docker build --target hererocks -t "$REGISTRY_PREFIX"hererocks .
+docker build --target lua5.1 -t "$REGISTRY_PREFIX"lua5.1 .
+docker build --target lua5.2 -t "$REGISTRY_PREFIX"lua5.2 .
+docker build --target lua5.3 -t "$REGISTRY_PREFIX"lua5.3 .
+docker build --target luajit2.0 -t "$REGISTRY_PREFIX"luajit2.0 .
+docker build --target luajit2.1 -t "$REGISTRY_PREFIX"luajit2.1 .
+docker build --target luacheck -t "$REGISTRY_PREFIX"luacheck .
+docker push "$REGISTRY_PREFIX"hererocks
+docker push "$REGISTRY_PREFIX"lua5.1
+docker push "$REGISTRY_PREFIX"lua5.2
+docker push "$REGISTRY_PREFIX"lua5.3
+docker push "$REGISTRY_PREFIX"luajit2.1
+docker push "$REGISTRY_PREFIX"luacheck
